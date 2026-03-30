b0dc0f6646b2d4ca774a4d7fc1ed1ad433dbd43d
haiderarman8 <haiderarman888@gmail.com>
2026-03-30 13:38:03 +0500

    created a trivia bot | JavaScript

---
+++ JavaScript/trivia bot/script.js
@@ -0,0 +1,28 @@
+// Display welcome message
+console.log("Hello! I'm your coding fun fact guide!");
+
+// Declare variables (all must be strings)
+let botName = "Zara"; // Bot's name
+let botLocation = "Lahore"; // Bot's location
+let favoriteLanguage = "JavaScript"; // Favorite programming language
+
+// Introduce the bot using variables
+console.log(`My name is ${botName} and I live in ${botLocation}.`);
+
+// Show favorite programming language
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+// Create first coding fact using let
+let codingFact = `${favoriteLanguage} is widely used for web development.`;
+console.log(codingFact); // Print first fact
+
+// Update codingFact with a new value
+codingFact = `${favoriteLanguage} can run both in the browser and on servers.`;
+console.log(codingFact); // Print second fact
+
+// Update codingFact again with another value
+codingFact = `${favoriteLanguage} is one of the most popular languages in the world.`;
+console.log(codingFact); // Print third fact
+
+// Goodbye message using variables
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
