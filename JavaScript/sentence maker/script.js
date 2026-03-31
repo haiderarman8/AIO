1e7595745a7eac93cfa56e56b49b399f3f54b57f
haiderarman8 <haiderarman888@gmail.com>
2026-03-31 19:16:36 +0500

    created a sentence maker | JavaScript

---
+++ JavaScript/sentence maker/script.js
@@ -0,0 +1,27 @@
+// Declare variables with initial values
+let adjective = "brave";
+let noun = "dragon";
+let verb = "flying";
+let place = "mountain";
+let adjective2 = "fiery";
+let noun2 = "marshmallows";
+
+// Create first story
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+// Log first story
+console.log(`First story: ${firstStory}`);
+
+// Reassign variables for second story
+adjective = "sneaky";
+noun = "cat";
+verb = "running";
+place = "city";
+adjective2 = "tiny";
+noun2 = "fish";
+
+// Create second story
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+// Log second story
+console.log(`Second story: ${secondStory}`);
