d6a4a931025572657e5dae78a5f2a40a3c7fa8b5
haiderarman8 <haiderarman888@gmail.com>
2026-04-03 23:47:53 +0500

    added comments to the code | JavaScript

--- JavaScript/greeting bot/script.js
+++ JavaScript/greeting bot/script.js
@@ -1,31 +1,47 @@
+// Display greeting messages
 console.log("Hi there!");
 console.log("I am excited to talk to you.");
+
+// Declare a variable for bot name
 let bot;
+
+// Assign initial value to bot
 bot = "teacherBot";
 
+// Declare and assign bot location
 let botLocation = "the universe";
 
+// Introduction message
 console.log("Allow me to introduce myself.");
 
+// Create introduction sentence using bot name
 const botIntroduction = "My name is " + bot + ".";
-console.log(botIntroduction);
+console.log(botIntroduction); // Print introduction
 
+// Create sentence for bot location
 const botLocationSentence = "I live in " + botLocation + ".";
-console.log(botLocationSentence);
+console.log(botLocationSentence); // Print location
 
+// Update bot name
 bot = "professorBot";
 
+// Create nickname introduction
 const nicknameIntroduction = "My nickname is " + bot + ".";
-console.log(nicknameIntroduction);
+console.log(nicknameIntroduction); // Print nickname
 
+// Update bot name again
 bot = "awesomeTeacherBot";
 
+// Create sentence with new nickname preference
 const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
-console.log(newNicknameGreeting);
+console.log(newNicknameGreeting); // Print new nickname message
 
+// Declare favorite subject
 const favoriteSubject = "Computer Science";
 
+// Create sentence for favorite subject
 const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
-console.log(favoriteSubjectSentence);
+console.log(favoriteSubjectSentence); // Print favorite subject
 
+// Goodbye message
 console.log("Well, it was nice to talk to you. Have a nice day!");
--- JavaScript/teacher chatbot/script.js
+++ JavaScript/teacher chatbot/script.js
@@ -1,43 +1,61 @@
+// Display greeting message
 console.log("Hi there!");
 
+// Declare bot name
 const botName = "teacherBot";
 
+// Create greeting using template literals
 const greeting = `My name is ${botName}.`;
-console.log(greeting);
+console.log(greeting); // Print greeting
 
+// Declare subject and topic
 const subject = "JavaScript";
 const topic = "strings";
 
+// Create sentence about today's lesson
 const sentence = `Today, you will learn about ${topic} in ${subject}.`;
-console.log(sentence);
+console.log(sentence); // Print lesson sentence
 
+// Introduce length property example
 const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
 console.log(strLengthIntro);
 
+// Print length of subject string
 console.log(subject.length);
 
+// Show length example for topic
 console.log(`Here is an example of using the length property on the word ${topic}.`);
-console.log(topic.length);
+console.log(topic.length); // Print length of topic
 
+// Access first character of subject
 console.log(`Here is an example of accessing the first letter in the word ${subject}.`);
+console.log(subject[0]); // Print first letter
 
-console.log(subject[0]);
-
+// Access second character of subject
 console.log(`Here is an example of accessing the second letter in the word ${subject}.`);
-console.log(subject[1]);
+console.log(subject[1]); // Print second letter
 
+// Access last character of subject
 console.log(`Here is an example of accessing the last letter in the word ${subject}.`);
 
+// Calculate last character using length
 const lastCharacter = subject[subject.length - 1];
-console.log(lastCharacter);
+console.log(lastCharacter); // Print last letter
 
+// Declare a sentence
 const learningIsFunSentence = "Learning is fun.";
 
+// Introduce indexOf examples
 console.log("Here are examples of finding the positions of substrings in the sentence.");
 
+// Find position of "Learning"
 console.log(learningIsFunSentence.indexOf("Learning"));
 
+// Find position of "fun"
 console.log(learningIsFunSentence.indexOf("fun"));
+
+// Try finding "learning" (case-sensitive, will return -1)
 console.log(learningIsFunSentence.indexOf("learning"));
 
+// Closing message
 console.log("I hope you enjoyed learning today.");
